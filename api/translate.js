b00e6815f35f48c80eb367f1b6f55db726d4527f
04280607 <130985446+04280607@users.noreply.github.com>
2026-04-08 22:32:17 +0800

    Add translation API handler in translate.js

---
+++ api/translate.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  const { text, target } = req.body
+
+  const appid = "20260408002589942"
+  const key = "p8vVSXAxIgBIZFxemo6D"
+  const salt = Date.now()
+
+  const crypto = await import('crypto')
+  const sign = crypto
+    .createHash('md5')
+    .update(appid + text + salt + key)
+    .digest('hex')
+
+  const url = `https://fanyi-api.baidu.com/api/trans/vip/translate?q=${encodeURIComponent(text)}&from=auto&to=${target}&appid=${appid}&salt=${salt}&sign=${sign}`
+
+  const response = await fetch(url)
+  const data = await response.json()
+
+  res.status(200).json({
+    translation: data.trans_result ? data.trans_result[0].dst : text
+  })
+}
